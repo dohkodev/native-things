41f245bd81d8acc5df40455ed43e76ef9c9dea52
Cristopher Vicuña <cvicuna@nectia.com>
2020-04-20 23:29:01 -0400

    navegando con navigator entre pantallas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = props => {
   return <View>
     <Text style={styles.text}>Hola Mundo!</Text>
     <Button 
       title="Ir al componente Demo"
-      onPress={() => console.log("boton demo!")}
+      onPress={() => props.navigation.navigate('Components')}
       />
     <TouchableOpacity
       onPress={() => console.log("boton lista!")}
